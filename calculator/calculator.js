4f76dc7412fb324f193b9a6203dbb0d7f61c8c72
Daniel Whalan <dwhalan1001@gmail.com>
2025-12-14 22:59:40 +1000

    Main page blockout

---
+++ calculator/calculator.js
@@ -0,0 +1,23 @@
+// Sets a variable of 'display' to elemenent with id 'display'
+const display = document.getElementById("display")
+
+function appendToDisplay(input){
+    if(display.value === "Error"){
+        clearDisplay()
+    }
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value)
+    }
+    catch(error){
+        display.value = "Error";
+    }
+
+}
