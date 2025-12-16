975a397c7577f7cd1eeac8fd157035782552645a
Daniel Whalan <dwhalan1001@gmail.com>
2025-12-17 07:25:01 +1000

    Flip 7 Player and scoring

--- flip7/flip7.js
+++ flip7/flip7.js
@@ -1,25 +1,30 @@
-let flip7PlayerCount = 3;
+let flip7PlayerCount = 0;
 
-function flip7AddPlayer(){
-    flip7PlayerCount++
-
+function flip7AddPlayer() {
+    const flip7PlayersToAdd = Number(document.getElementById("flip7-player-count").value);
     const container = document.getElementById("flip7-players-container");
+    let flip7NewPlayerCount = 0;
 
-    const playerID = `player-${flip7PlayerCount}`;
+    for (; flip7NewPlayerCount < flip7PlayersToAdd; flip7PlayerCount++) {
+        flip7NewPlayerCount++
+        const playerID = `player-${flip7PlayerCount + 1}`;
+        const row = document.createElement("div");
 
-    const row = document.createElement("div");
-    row.id = `flip7-${playerID}`;
-    row.className = "flip7-player";
+        row.id = `flip7-${playerID}`;
+        row.className = "flip7-player";
+        row.innerHTML = `
+            <input id="flip7-${playerID}-name" class="flip7-player-name">
+            <input id="flip7-${playerID}-total" type="number" value="0" class="flip7-player-total" readonly>
+            <input id="flip7-${playerID}-hand" type="number" class="flip7-player-hand">
+            <button type="button" onclick="flip7AddScore('${playerID}')" class="flip7-add-score">+</button>
+        `;
 
-    row.innerHTML = `
-        <input id="flip7-${playerID}-name" class="flip7-player-name">
-        <input id="flip7-${playerID}-total" type="number" value="0" class="flip7-player-total" readonly>
-        <input id="flip7-${playerID}-hand" type="number" class="flip7-player-hand">
-        <button onclick="flip7AddScore('${playerID}')" class="flip7-add-score">+</button>
-    `;
+        container.appendChild(row);
+        }
+    }
 
-    container.appendChild(row);
-}
+
+
 
 function flip7AddScore(playerID){
     const player_total = document.getElementById(`flip7-${playerID}-total`);
